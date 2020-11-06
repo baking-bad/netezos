3c08f5eeebe7a78c771ea53379bfb49965ce8e68
Alex <lesha.erohin@mail.ru>
2020-11-06 17:36:50 +0300

    Added PublicKeyHashesTests

--- Netezos.Tests/Keys/KeysTests.cs
+++ Netezos.Tests/Keys/KeysTests.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using System.IO;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Netezos.Keys;
 using Netezos.Rpc;
+using System.Text.Json;
 using Xunit;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace Netezos.Tests.Keys
 {
@@ -14,6 +18,25 @@ namespace Netezos.Tests.Keys
             var key1 = new Key(ECKind.Ed25519);
             var key2 = new Key(ECKind.NistP256);
             var key3 = new Key(ECKind.Secp256k1);
+        }
+
+        [Fact]
+        public async Task TestPublicKeyHashes()
+        {
+            var publicKeyHashesPath = @"C:\Users\lesha\OneDrive\Рабочий стол\Baking Bad\keys\public_key_hashs";
+            var secretKeysPath = @"C:\Users\lesha\OneDrive\Рабочий стол\Baking Bad\keys\secret_keys";
+
+            var addressPkH = await File.ReadAllTextAsync(publicKeyHashesPath);
+            var addressSkP = await File.ReadAllTextAsync(secretKeysPath);
+            var publicKeyHashesList = JsonSerializer.Deserialize<List<GeneratedKeys>>(addressPkH);
+            var secretKeysList = JsonSerializer.Deserialize<List<GeneratedKeys>>(addressSkP);
+
+            foreach (var item in publicKeyHashesList)
+            {
+                var objectSecretKeys = secretKeysList.FirstOrDefault(x => x.name == item.name);
+                var key = Key.FromBase58(objectSecretKeys.value.Remove(0, 12));
+                Assert.True(item.value == key.PubKey.Address);
+            }
         }
     }
 }
