869a20ada8f22ec47b900f0fbc010b8d7521360f
Sergey Bokoch <bokoch98@yandex.ru>
2021-07-12 00:54:31 +0300

    added chain and config queries

---
+++ Netezos/Rpc/Queries/ChainBlocksQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace Netezos.Rpc.Queries
+{
+    public class ChainBlocksQuery : RpcObject
+    {
+        internal ChainBlocksQuery(RpcClient client, string query) : base(client, query)
+        {
+        }
+    }
+}
---
+++ Netezos/Rpc/Queries/ChainQuery.cs
@@ -0,0 +1,41 @@
+﻿namespace Netezos.Rpc.Queries
+{
+    /// <summary>
+    /// Forcefully set the bootstrapped flag of the node
+    /// </summary>
+    public class ChainQuery : RpcObject
+    {
+        /// <summary>
+        /// Lists block hashes from '', up to the last checkpoint, sorted with decreasing fitness.
+        /// Without arguments it returns the head of the chain. Optional arguments allow to return the list of predecessors of a given block or of a set of blocks.
+        /// </summary>
+        public RpcObject Blocks => new RpcObject(this, "blocks/");
+
+        /// <summary>
+        /// The current checkpoint for this chain.
+        /// </summary>
+        public RpcObject Checkpoint => new RpcObject(this, "checkpoint/");
+
+        /// <summary>
+        /// Lists blocks that have been declared invalid along with the errors that led to them being declared invalid.
+        /// </summary>
+        public RpcObject InvalidBlocks => new RpcObject(this, "invalid_blocks/");
+
+        /// <summary>
+        /// The chain unique identifier.
+        /// </summary>
+        public RpcObject ChainId => new RpcObject(this, "chain_id/");
+
+        /// <summary>
+        /// The bootstrap status of a chain
+        /// </summary>
+        public RpcObject IsBootstrapped => new RpcObject(this, "is_bootstrapped/");
+
+        /// <summary>
+        /// Gets the query to the context associated with the block
+        /// </summary>
+        public RpcObject Mempool => new RpcObject(this, "mempool/");
+
+        internal ChainQuery(RpcQuery baseQuery, string append) : base(baseQuery, append) { }
+    }
+}
