94f30c70cee380e95ca8bdccbf9077f0069f0c12
dmir <demonmir@inbox.ru>
2021-09-06 19:21:53 +0300

    HDKeyTests refactoring

--- Netezos.Tests/Keys/HDKeys/HDKeyTests.cs
+++ Netezos.Tests/Keys/HDKeys/HDKeyTests.cs
@@ -104,53 +104,26 @@ namespace Netezos.Tests.Keys
         }
         
         [Fact]
-        public void Kukai()
+        public void Atomex()
         {
-            //The same for Temple
-            const string mnemonic = "find load relief loop surround tired document coin seven filter draft place music jewel match shoe hope duty thumb cereal lyrics embody talent lumber";
-            const string mnemonic1 =
-                "guide,dove,reform,mesh,grant,gold,process,fiscal,popular,odor,decorate,bright,  clock,human,  enough,inquiry,upper,hire,foster,law,canal,jeans,pact,push";
-            const string address = "tz1dP3E6Pa7Yp8wTeN2CPKfDr5ueLQwiDDTy";
-            const string secondAddress = "tz1NWFkgmi2aQRkkjcs1yVY18U1xRrPyWiWA";
-
-            var key = HDKey.FromMnemonic(Mnemonic.Parse(mnemonic)).Derive(HDPath.Parse("m/44'/1729'/0'/0'"));
-            var key1 = HDKey.FromMnemonic(Mnemonic.Parse(mnemonic1)).Derive(HDPath.Parse("m/44'/1729'/0'/0'"));
-            Assert.Equal(address, key.PubKey.Address);
-            Assert.Equal("tz1R8HydaqYsfBE6NhzqAddpYepEuhSh6WBB", key1.PubKey.Address);
-
-            var secondKey = HDKey.FromMnemonic(Mnemonic.Parse(mnemonic)).Derive(HDPath.Parse("m/44'/1729'/1'/0'"));
-            var secondKey1 = HDKey.FromMnemonic(Mnemonic.Parse(mnemonic1)).Derive(HDPath.Parse("m/44'/1729'/1'/0'"));
-            Assert.Equal(secondAddress, secondKey.PubKey.Address);
-            Assert.Equal("tz1h2PMMKPJq2hg55RJ7XDvR3bBFLEhEq1Vx", secondKey1.PubKey.Address);
+            foreach (var sample in DJson.Read(@"..\..\..\Keys\HDKeys\Samples\atomex.json"))
+            {
+                var key = HDKey.FromMnemonic(Mnemonic.Parse((string)sample.mnemonic)).Derive((string)sample.path);
+            
+                Assert.Equal(sample.privateKey, key.Key.GetBase58());
+                Assert.Equal(sample.address, key.PubKey.Address);
+            }
         }
         
         [Fact]
-        public void Atomex()
+        public void Kukai()
         {
-            //The same for Temple
-            const string mnemonic = "blossom kite abuse predict remember acquire useful rifle situate polar noodle retreat surround turn exotic human push depart uncover nut wise snow pulp filter";
+            foreach (var sample in DJson.Read(@"..\..\..\Keys\HDKeys\Samples\kukai.json"))
+            {
+                var key = HDKey.FromMnemonic(Mnemonic.Parse((string)sample.mnemonic)).Derive((string)sample.path);
             
-            const string address = "tz1VmJrHivDEppXkyNfmVm9L1iAskMNP9x9U";
-            const string secondAddress = "tz1MYSt3kAzNmvRK3Xo32hUwBzQMpRDGFsPH";
-            const string thirdAddress = "tz1VAbTH5i9xzqd6yy1xfvrYcgp9EcQScxAn";
-
-            const string firstPriv = "edsk3QsgTz7AcEsmY8HZS4D164thNvttMvLMbcgvWSXCv4Ee6z51By";
-            const string secondPriv = "edsk4AfFtwD79KM1Uxcsop41TrMYWFGvKEM8zKbYSrYMQLFT9uPqS7";
-            const string thirdPriv = "edsk3C4ZFwWj92JmzFUQMoSirVCM15jdrfQ6ZbjFxHMzRK7Z4Cw8wa";
-            
-
-            var key = HDKey.FromMnemonic(Mnemonic.Parse(mnemonic)).Derive(HDPath.Parse("m/44'/1729'/0'/0'"));
-            Assert.Equal(address, key.PubKey.Address);
-            //TODO HDKey Key should return proper value
-            Assert.Equal(firstPriv, Base58.Convert(key.GetBytes().GetBytes(0, 32), new byte[]{ 13, 15, 58, 7 }));
-
-            var secondKey = HDKey.FromMnemonic(Mnemonic.Parse(mnemonic)).Derive(HDPath.Parse("m/44'/1729'/1'/0'"));
-            Assert.Equal(secondAddress, secondKey.PubKey.Address);
-            Assert.Equal(secondPriv, Base58.Convert(secondKey.GetBytes().GetBytes(0, 32), new byte[]{ 13, 15, 58, 7 }));
-
-            var thirdKey = HDKey.FromMnemonic(Mnemonic.Parse(mnemonic)).Derive(HDPath.Parse("m/44'/1729'/2'/0'"));
-            Assert.Equal(thirdAddress, thirdKey.PubKey.Address);
-            Assert.Equal(thirdPriv, Base58.Convert(thirdKey.GetBytes().GetBytes(0, 32), new byte[]{ 13, 15, 58, 7 }));
+                Assert.Equal(sample.address, key.PubKey.Address);
+            }
         }
     }
 }
