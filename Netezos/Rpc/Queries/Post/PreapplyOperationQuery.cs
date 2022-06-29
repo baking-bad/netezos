8d983c0b3ee1d56b72811233ded4b199670a8140
257Byte <257Byte@gmail.com>
2022-06-29 20:13:31 +0300

    Extend preapply with multiple operations

--- Netezos/Rpc/Queries/Post/PreapplyOperationQuery.cs
+++ Netezos/Rpc/Queries/Post/PreapplyOperationQuery.cs
@@ -1,4 +1,6 @@
+using Netezos.Forging.Models;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Netezos.Rpc.Queries.Post
@@ -13,12 +15,25 @@ namespace Netezos.Rpc.Queries.Post
         /// <summary>
         /// Simulates the validation of an operation and returns the preapplied data
         /// </summary>
+        /// <param name="operations">Operations</param>
+        /// <returns></returns>
+        public Task<dynamic> PostAsync(string protocol, IEnumerable<Operation> operations)
+            => PostAsync(operations.Select(op => new {
+                protocol,
+                signature = op.Signature,
+                branch = op.Branch,
+                contents = op.Contents
+            }));
+
+        /// <summary>
+        /// Simulates the validation of an operation and returns the preapplied data
+        /// </summary>
         /// <param name="branch">Branch</param>
         /// <param name="protocol">Protocol hash</param>
         /// <param name="signature">Signature</param>
         /// <param name="contents">List of contents</param>
         /// <returns></returns>
-        public Task<dynamic> PostAsync(string protocol, string signature, string branch, List<object> contents)
+        public Task<dynamic> PostAsync(string protocol, string signature, string branch, List<OperationContent> contents)
             => PostAsync(new[]
             {
                 new
@@ -39,7 +54,7 @@ namespace Netezos.Rpc.Queries.Post
         /// <param name="signature">Signature</param>
         /// <param name="contents">List of contents</param>
         /// <returns></returns>
-        public Task<T> PostAsync<T>(string protocol, string signature, string branch, List<object> contents)
+        public Task<T> PostAsync<T>(string protocol, string signature, string branch, List<OperationContent> contents)
             => PostAsync<T>(new[]
             {
                 new
